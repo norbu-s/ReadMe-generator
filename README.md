7966f594f8027ed3c78d429e4def62a27decdb58
Norbu Sherpa <norbuts@hotmail.com>
2021-02-08 22:49:56 +1100

    Initial files creation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ReadMe-generator
+# ReadMe-Generator
+
+# Description
+
+When creating an open source project on GitHub, it is important to have a quality README with information about the app--what is the app for, how to use the app, how to install it, how to report issues, and how to make contributions so that other developers are more likely to use and contribute to the success of the project. A command-line application will allow for quick and easy generation of a project README to get started quickly. This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
+
+# User Story
+AS A developer
+I WANT a README generator
+SO THAT can quickly create a professional README for a new project
+
+# Table of Contents
+
+
+# Installation
+ In order to use the application, the user will need to have node.js installed and then will need to clone the repository into the command line. Once the repository has been cloned, the user will need to change directories into the repository and run the command "npm install" in order to retrieve the neccessary dependencies.
+
+# Usage
+The application is designed to run within the command-line. Once the user has installed the program and run the "npm install" command, they will need to run "npm start" to begin the prompts regarding the README content. The user will then need to answer the questions accordingly and at the end they will receive a success message and a README will be generated within the folder.
---
+++ index.js
@@ -0,0 +1,98 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type:'input',
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is the description of your project?",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "How do you install your project?",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "What is the usage information?",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "What are the contribution guidelines?",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "What are the test instructions?",
+            name: "tests"
+        },
+        {
+            type: "list",
+            message: "Please select a type of license.",
+            name: "license",
+            choices: [
+                "MIT",
+                "Apache",
+                "ISC",
+                "GNU GPLv3"
+            ]
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email"
+        },
+    ]);
+};
+
+const generateHTML = (answers) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<title>Document</title>
+</head>
+<body>
+<div class="jumbotron jumbotron-fluid">
+<div class="container">
+  <h1 class="display-4">Title: ${answers.title}</h1>
+  <p class="title"> Project Description: ${answers.title}.</p>
+  <p class="description"> Installation: ${answers.installation}.</p>
+  <p class="description"> Usage: ${answers.usage}.</p>
+  <p class="description"> Contribution: ${answers.contribution}.</p>
+  <p class="description"> Tests: ${answers.tests}.</p>
+  <p class="description"> License Description: ${answers.choices}.</p>
+  <p class="description"> Github Username: ${answers.username}.</p>
+  <p class="description"> Contact Info: ${answers.email}.</p>
+</ul>
+</div>
+</div>
+</body>
+</html>`;
+
+const init = () => {
+    promptUser()
+      .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err));
+  };
+  
+  init();
